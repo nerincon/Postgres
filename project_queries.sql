e7c88c0d8e8c974bd7a897d165ea805445135181
nerincon <nerincon1@gmail.com>
2018-04-04 00:53:47 -0500

    finish 2-3 exer

--- project_queries.sql
+++ project_queries.sql
@@ -2,3 +2,13 @@ SELECT project.name FROM project
   JOIN project_uses_tech ON project_uses_tech.project_id = project.id
   JOIN tech ON project_uses_tech.tech_id = tech.id
   WHERE tech.name='Javascript';
+
+SELECT tech.name FROM tech
+  JOIN project_uses_tech ON project_uses_tech.tech_id = tech.id
+  JOIN project ON project_uses_tech.project_id = project.id
+  WHERE project.name='Personal Website';
+
+SELECT name FROM tech
+  LEFT OUTER JOIN project_uses_tech
+    ON project_uses_tech.tech_id = tech.id
+    WHERE project_id IS NULL;
